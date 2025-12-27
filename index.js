07d3ef9af7e95253037425de6a70d7b85bb2865b
Kushagra Tiwari <kushagratiwari24@gmail.com>
2025-12-27 13:05:24 +0530

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Auto-Deployed Website 🚀</h1>");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
